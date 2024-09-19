eff795a2358cfb0d8c38de5dd8f9a6d6c1bf75e8
felix-wang-0307 <hwang779@usc.edu>
2024-09-19 00:46:35 -0700

    feat: main.py to add API endpoints for weather, geocoding, and querying; add state mappings

--- main.py
+++ main.py
@@ -1,17 +1,104 @@
-from flask import Flask
-from google.cloud import datastore
+from flask import Flask, request
 import json
+import service
 
 app = Flask(__name__)
-datastore_client = datastore.Client()
+WEATHER_API = "https://api.tomorrow.io/v4/weather/forecast"
+
+STATE_ABBREVIATIONS = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+}
+
 
 @app.route("/")
 def root():
-    return json.dumps({"success": True, "data": {"a": 2}})
+    return json.dumps(
+        {
+            "success": True,
+            "data": {"text": "this is a sample text"},
+            "apis": [
+                "GET /weather?latitude={latitude}&longitude={longitude}",
+                "GET /address?address={address}",
+            ],
+        }
+    )
 
-@app.route("/api/v1/data")
-def get_data():
-    return json.dumps({"success": True, "data": {"a": 2}})
+
+@app.route("/query", methods=["GET"])
+def query():
+    street = request.args.get("street")
+    city = request.args.get("city")
+    state = request.args.get("state")
+    return json.dumps({"success": True, "data": request.args})
+
+@app.route("/state_list", methods=["GET"])
+def state_list_controller():
+    return json.dumps({"success": True, "data": STATE_ABBREVIATIONS})
+
+@app.route("/weather", methods=["GET"])
+def weather_controller():
+    latitude = request.args.get("latitude")
+    longitude = request.args.get("longitude")
+    return service.get_weather(latitude, longitude)
+
+
+@app.route("/geocoding", methods=["GET"])
+def geocoding_controller():
+    address = request.args.get("address")
+    return service.get_address_coordinates(address)
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
