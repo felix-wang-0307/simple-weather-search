1d18fa7cd0999bd4f355f4fd2ecd0b003425b724
felix-wang-0307 <hwang779@usc.edu>
2024-09-19 00:47:18 -0700

    feat: isolate the service api calls

---
+++ service.py
@@ -0,0 +1,53 @@
+import json
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+WEATHER_API = "https://api.tomorrow.io/v4/weather/forecast"
+
+
+def get_weather(latitude, longitude):
+    if latitude is None or longitude is None:
+        return (
+            json.dumps({"success": False, "error": "Missing latitude or longitude"}),
+            400,
+        )
+
+    response = requests.get(
+        WEATHER_API,
+        params={
+            "apikey": os.getenv("TOMORROW_API_KEY"),
+            "location": f"{latitude},{longitude}",
+        },
+    )
+
+    if response.status_code != 200:
+        return (
+            json.dumps({"success": False, "error": "Failed to fetch weather data"}),
+            500,
+        )
+
+    return json.dumps({"success": True, "data": response.json()})
+
+
+def get_address_coordinates(address):
+    response = requests.get(
+        "https://maps.googleapis.com/maps/api/geocode/json",
+        params={"address": address, "key": os.getenv("GOOGLE_MAPS_API_KEY")},
+    )
+
+    if response.status_code != 200:
+        return (
+            json.dumps({"success": False, "error": "Failed to fetch address info"}),
+            500,
+        )
+    
+    coordinates = response.json().get("results")[0].get("geometry").get("location")
+    return json.dumps({"success": True, "data": coordinates})
+
+
+if __name__ == "__main__":
+    print(get_weather(34.0522, -118.2437))
+    print(get_address_coordinates("University of Southern California"))
