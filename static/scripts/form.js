085de4072bb292563e6ffb1e34d40a46d354b849
felix-wang-0307 <hwang779@usc.edu>
2024-09-19 23:55:20 -0700

    style: Refactor form logic and add form validation

--- static/scripts/form.js
+++ static/scripts/form.js
@@ -1,4 +1,12 @@
 
+function getFormItems() {
+  const street = document.getElementById('street').value;
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+  const autoDetect = document.getElementById('auto-detect').checked;
+  return { street, city, state, autoDetect };
+}
+
 function submitForm(event) {
   event.preventDefault();
   const street = document.getElementById('street').value;
@@ -14,11 +22,22 @@ function submitForm(event) {
   // You can now use these values to perform further actions, such as sending them to a server
 }
 
-
 function clearForm() {
   console.log("Clearing form");
   document.getElementById('street').value = '';
   document.getElementById('city').value = '';
   document.getElementById('state').value = '';
   document.getElementById('auto-detect').checked = false;
+  toggleAutoDetect();
+}
+
+function toggleAutoDetect() {
+  const isAutoDetect = document.getElementById('auto-detect').checked;
+  const street = document.getElementById('street');
+  const city = document.getElementById('city');
+  const state = document.getElementById('state');
+
+  street.disabled = isAutoDetect;
+  city.disabled = isAutoDetect;
+  state.disabled = isAutoDetect;
 }
