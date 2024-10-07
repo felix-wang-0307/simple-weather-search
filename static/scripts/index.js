8dfc3809fb04ee43563c3b4fe7278fbdc4dfc60d
felix-wang-0307 <hwang779@usc.edu>
2024-10-07 03:23:04 -0700

    feat(index.js): Implement toggle display feature for weather charts

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,6 +1,20 @@
-import { clearForm, submitForm, toggleAutoDetect } from "./form.js"
+import { clearForm, submitForm, toggleAutoDetect } from "./form.js";
 
 document.getElementById("weather-form").addEventListener('submit', submitForm);
 document.getElementById("weather-form").addEventListener('reset', clearForm);
 document.getElementById("auto-detect").addEventListener('click', toggleAutoDetect);
 
+const pointDown = document.querySelector(".point-down");
+const pointUp = document.querySelector(".point-up");
+
+pointDown.addEventListener('click', () => {
+  pointDown.style.display = "none";
+  pointUp.style.display = "flex";
+  document.querySelector("#weather-charts .charts").style.display = "flex";
+});
+
+pointUp.addEventListener('click', () => {
+  pointUp.style.display = "none";
+  pointDown.style.display = "flex";
+  document.querySelector("#weather-charts .charts").style.display = "none";
+});
