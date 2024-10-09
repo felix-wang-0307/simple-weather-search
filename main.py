5fef6977de0f75be14f4b07171eed52c5f981a65
felix-wang-0307 <hwang779@usc.edu>
2024-10-09 01:32:12 -0700

    refactor(main.py): Update weather and geocoding controllers to return JSON response and status code

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import service
 import os
 
 app = Flask(__name__)
-WEATHER_API = "https://api.tomorrow.io/v4/weather/forecast"
 
 with open('./static/states.json') as f:
     STATE_ABBREVIATIONS = json.load(f)
@@ -28,31 +27,19 @@ def static_files(path):
         return redirect("/")
 
 
-@app.route("/query", methods=["GET"])
-def query():
-    street = request.args.get("street")
-    city = request.args.get("city")
-    state = request.args.get("state")
-    return jsonify({"success": True, "data": request.args})
-
-
-@app.route("/state_list", methods=["GET"])
-def state_list_controller():
-    return jsonify({"success": True, "data": STATE_ABBREVIATIONS})
-
-
 @app.route("/weather", methods=["GET"])
 def weather_controller():
     latitude = request.args.get("latitude")
     longitude = request.args.get("longitude")
-    return service.get_weather(latitude, longitude)
+    (res, status_code) = service.get_weather(latitude, longitude)
+    return jsonify(res), status_code
 
 
 @app.route("/geocoding", methods=["GET"])
 def geocoding_controller():
     address = request.args.get("address")
-    return service.get_geocode_info(address)
-
+    (res, status_code) = service.get_geocode_info(address)
+    return jsonify(res), status_code
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
