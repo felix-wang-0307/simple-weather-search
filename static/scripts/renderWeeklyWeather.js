7f7c1aa5d4cfe522edb8837f1e7a0d75ae328ecc
felix-wang-0307 <hwang779@usc.edu>
2024-10-14 12:50:50 -0700

    docs: add necessary comments

--- static/scripts/renderWeeklyWeather.js
+++ static/scripts/renderWeeklyWeather.js
@@ -9,9 +9,12 @@ export async function renderCurrentWeather(locationString, weather) {
   const weatherCodeMapping =
       await fetch("/scripts/weatherCodes.json").then(response => response.json());
   const { desc, icon } = weatherCodeMapping[weather.weatherCode];
+  // Set the location
   document.querySelector(".formatted-address").textContent = locationString;
+  // Set the weather description and icon
   document.getElementById("weather-icon").src = icon;
   document.getElementById("weather-desc").textContent = desc;
+  // Set the weather fields
   document.getElementById("temperature").textContent = weather.temperature;
   document.getElementById("humidity").textContent = weather.humidity + "%";
   document.getElementById("pressure").textContent = weather.pressureSeaLevel + "inHg";
@@ -21,15 +24,18 @@ export async function renderCurrentWeather(locationString, weather) {
   document.getElementById("uv-level").textContent = weather.uvIndex;
 }
 
+// Render the weekly weather in 6 or 7 rows. Each row contains the following columns:
+// Date, Weather Status, Max Temperature, Min Temperature, Wind Speed
 export async function renderWeeklyWeather(weeklyWeather) {
   const weeklyWeatherDiv = document.getElementById("weekly-weather");
   if (weeklyWeatherDiv.childElementCount > 1) {
-    // only reserve the first child (table header)
+    // only reserve the first child (table header) and remove the rest children
+    // to avoid duplicate rendering (e.g., when the user clicks on the search button multiple times)
     weeklyWeatherDiv.innerHTML = weeklyWeatherDiv.firstElementChild.outerHTML;
   }
   const weatherCodeMapping =
       await fetch("/scripts/weatherCodes.json").then(response => response.json());
-
+  // Create a column
   function createWeatherStatusColumn(dailyWeather, weatherCodeMapping) {
     const weatherDescDiv = createDivElement("weather-item weather-status", "");
     const { desc, icon } = weatherCodeMapping[dailyWeather.values.weatherCode];
@@ -40,7 +46,7 @@ export async function renderWeeklyWeather(weeklyWeather) {
     weatherDescDiv.appendChild(createDivElement("weather-desc", desc));
     return weatherDescDiv;
   }
-
+  // Create a row
   function createWeatherRow(dailyWeather) {
     const weatherRowDiv = createDivElement("weather-row", "");
     weatherRowDiv.appendChild(createDivElement("weather-item", convertDate(dailyWeather.startTime)));
@@ -53,6 +59,7 @@ export async function renderWeeklyWeather(weeklyWeather) {
 
   for (const dailyWeather of weeklyWeather) {
     const weatherRowDiv = createWeatherRow(dailyWeather);
+    // Add a click event listener to each row
     weatherRowDiv.onclick = () => {
       hideWeatherDisplay();
       renderDailyDetails(dailyWeather);
