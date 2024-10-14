97488e2c2588b8596bcc5dab19aa5d7c6d74acdd
felix-wang-0307 <hwang779@usc.edu>
2024-10-14 12:50:31 -0700

    fix: fix the date format to match the requirement

--- static/scripts/utils.js
+++ static/scripts/utils.js
@@ -7,7 +7,10 @@ export function createDivElement(className, textContent) {
 
 export function convertDate(ISODate) {
   const date = new Date(ISODate);
-  // Transfer the ISODate to "Wednesday, 1 Jan 2020" format
-  return date.toLocaleDateString("en-US", {weekday: "long", day: "2-digit", month: "short", year: "numeric"});
+  // Transfer the ISODate to "Wednesday, 1 Jan, 2020" format
+  const localeString = date.toLocaleDateString("en-US", {weekday: "long", day: "2-digit", month: "short", year: "numeric"});
+  // Remove the 2nd comma between the Month and Year
+  const secondCommaIndex = localeString.indexOf(",", localeString.indexOf(",") + 1);
+  return localeString.slice(0, secondCommaIndex) + localeString.slice(secondCommaIndex + 1);
 }
 
